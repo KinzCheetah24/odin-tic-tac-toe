f8df46e5a90e0415b1677a2ca2d3adda2da4fee8
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-07-28 16:51:03 +0200

    The javascript code for the display logic was added
    The code for implementing the logic for playing the rounds through the display was completed.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function createPlayer(marker) {
-    const player = marker;
+function createPlayer(name, marker) {
+    const player = {name, marker};
 
-    const printPlayer = () => console.log("Player: " + player);
+    const printPlayer = () => console.log("Player: " + name + " Marker: " +  marker);
 
-    const getMarker = () => {return marker};
+    const setName = (newName) => player.name = newName;
 
-    return {printPlayer, getMarker};
+    const getName = () => {return player.name};
+
+    const getMarker = () => {return player.marker};
+
+    return {printPlayer, setName, getName, getMarker};
 };
 
-const playerX = createPlayer('X');
-const playerO = createPlayer('O');
+const playerX = createPlayer("PlayerX", 'X');
+const playerO = createPlayer("PlayerO", 'O');
 
 const Gameboard = (function () {
     const board = ['','','','','','','','',''];
@@ -20,6 +24,8 @@ const Gameboard = (function () {
         console.log("[" + board[3] + "] " + "[" + board[4] + "] " + "[" + board[5] + "] ");
         console.log("[" + board[6] + "] " + "[" + board[7] + "] " + "[" + board[8] + "] ");
     };
+
+    const getBoard = () => {return board};
 
     const clearBoard = () => {
         for (let i = 0 ; i < board.length ; i++) {
@@ -65,55 +71,80 @@ const Gameboard = (function () {
         return winner;
     };
 
-    return {printBoard, clearBoard, setSquare, isBoardFull, isBoardWinner};
+    return {printBoard, getBoard, clearBoard, setSquare, isBoardFull, isBoardWinner};
 })();
 
 const GameController = (function () {
-    const startGame = () => {
-        let gameDone = false;
-        let playerTurn = false;
+    let gameOver = false;
+    let currentPlayer = playerX;
 
-        while (!gameDone) {
-            let positionValid = 0, index = -1;
-            if(playerTurn) {
-                index = prompt("Turn for playerO, please introduce a position(0-8): ");
-                positionValid = Gameboard.setSquare(index, playerO.getMarker());
-
-                while (positionValid === -1) {
-                    index = prompt("Introduce another position: ");
-                    positionValid = Gameboard.setSquare(index, playerO.getMarker());
-                }
-            } else {
-                index = prompt("Turn for playerX, please introduce a position(0-8): ");
-                positionValid = Gameboard.setSquare(index, playerX.getMarker());
-
-                while (positionValid === -1) {
-                    index = prompt("Introduce another position: ");
-                    positionValid = Gameboard.setSquare(index, playerX.getMarker());
-                }
+    const playRound = (index) => {
+        if(!gameOver) {
+            if (Gameboard.setSquare(index, currentPlayer.getMarker()) === 0) {
+                DisplayDomLogic.render();
+                currentPlayer = currentPlayer === playerX ? playerO : playerX; 
             }
 
-            Gameboard.printBoard();
-
-            playerTurn = !playerTurn;
-
             if (Gameboard.isBoardWinner() !== null) {
-                console.log("The game has ended: Player" + Gameboard.isBoardWinner() + " wins");
-                gameDone = true;
+                gameOver = true;
+
+                return "The game has ended: " + (Gameboard.isBoardWinner() === "X" ? playerX.getName() : playerO.getName()) + " wins";
             } else {
                 if (Gameboard.isBoardFull()) {
-                    console.log("The game has ended: TIE");
-                    gameDone = true;
+                    gameOver = true;
+
+                    return "The game has ended: TIE";
                 }
             }
         }
+        return null;
     };
 
     const resetGame = () => {
+        gameOver = false;
+        currentPlayer = playerX;
         Gameboard.clearBoard();
+        DisplayDomLogic.render()
+        DisplayDomLogic.updateStatus(null);
     };
 
-    return {startGame, resetGame};
+    return {playRound, resetGame};
 })();
 
-GameController.startGame();
+const DisplayDomLogic = (function () {
+    const playerXName = document.querySelector(".playerXName");
+    const playerOName = document.querySelector(".playerOName");
+    const squares = document.querySelector(".gameboard").children;
+    const status = document.querySelector(".status");
+    const restartBtn = document.querySelector(".restartBtn");
+
+    const render = () => {
+        const board = Gameboard.getBoard();
+        for (let i = 0 ; i < squares.length ; i++) {
+            squares[i].textContent = board[i];
+        }
+    };
+
+    const updateStatus = (message) => {
+        status.textContent = message;
+    };
+
+    for (let i = 0 ; i < squares.length ; i++) {
+        squares[i].addEventListener("click", () => {
+            const index = i;
+            let roundResult = GameController.playRound(index);
+            render();
+
+            if(roundResult !== null) {
+                updateStatus(roundResult);
+            }
+        });
+    }
+
+    restartBtn.addEventListener("click", () => GameController.resetGame());
+
+    playerXName.addEventListener("input", (e) => playerX.setName(e.target.value));
+    playerOName.addEventListener("input", (e) => playerO.setName(e.target.value));
+    
+    return {render, updateStatus};
+})();
